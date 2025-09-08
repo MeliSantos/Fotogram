f2e3b38e4fde116fb6bb55fce7d5740cc9ed5955
Ellen Santos <ellensantos10@hotmail.com>
2025-09-08 17:52:02 +0200

    dialog updated

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const dialogRef = document.getElementById("Dialog_rio_img");
+ let dialogIds = [
+    'Dialog_rio_img',
+    'Dialog_dune_img',
+    'Dialog_Algarve',
+    'Dialog_Piedade',
+    'Dialog_Yucatán',
+    'Dialog_Maui',
+    'Dialog_greece',
+    'Dialog_Votsalo',
+    'Dialog_Beach',
+    'Dialog_Bali',
+    'Dialog_Cave',
+    'Dialog_Shellfish'
+  ];
 
 
 function openDialog(dialogId) {
@@ -10,3 +23,19 @@ function closeDialog(dialogId) {
     const dialog = document.getElementById(dialogId);
     if (dialog) dialog.close();
 }
+
+function showNext(currentId) {
+    const currentIndex = dialogIds.indexOf(currentId);
+    if (currentIndex === -1) return;
+    const nextIndex = (currentIndex + 1) % dialogIds.length;
+    closeDialog(currentId);
+    openDialog(dialogIds[nextIndex]);
+  }
+
+  function showPrevious(currentId) {
+    const currentIndex = dialogIds.indexOf(currentId);
+    if (currentIndex === -1) return;
+    const prevIndex = (currentIndex - 1 + dialogIds.length) % dialogIds.length;
+    closeDialog(currentId);
+    openDialog(dialogIds[prevIndex]);
+  }
